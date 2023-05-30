c1ae8521ab8ec4e874f6e1a496ddc8999882af97
Uverit <web.newhorizon@gmail.com>
2023-05-30 22:55:33 +0200

    v0.4.3 - Image Upload
    Added an Image Upload functionality.
    The user can drag & drop an image (only specific types - currently it's jpeg, png and svg) or upload via input (button).
    
    There are multiple icons to indicate the current state (like wrong file and successful upload).
    
    The user can also remove the uploaded image.

--- int_resources/js/sections.js
+++ int_resources/js/sections.js
@@ -208,3 +208,157 @@ addLinkBtn?.addEventListener("click", function () {
         linkIndex++;
     }
 });
+//*--|*|--*\\_____// Image Upload \\_____//*--|*|--*\\
+// Upload Icon
+const uploadIcon = "<svg class='modal-icon lm-upload-icon' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'><defs><style>.cls-1{fill:none;stroke-linecap:round;stroke-linejoin:round;}</style></defs><circle class='cls-1' cx='100' cy='100' r='90'/><path class='cls-1 lm-upload-inner' d='M145.6,116.6v15.9a15,15,0,0,1-15,15H69.4a15,15,0,0,1-15-15V116.6'/><line class='cls-1 lm-upload-inner lm-upload-arrow lm-upload-arrowshaft' x1='100' y1='121.9' x2='100' y2='52.5'/><polyline class='cls-1 lm-upload-inner lm-upload-arrow' points='124.7 77.2 100 52.5 75.3 77.2'/></svg>";
+// Checkmark Icon
+const checkmarkIcon = "<svg class='modal-icon lm-check-icon' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'><defs><style>.cls-1{fill:none;stroke-linecap:round;stroke-linejoin:round;}</style></defs><circle class='cls-1 lm-check-circle' cx='100' cy='100' r='90'/><polyline class='cls-1 lm-checkmark' points='51.9 103 84.1 135.3 148.1 71.3'/></svg>";
+// X Icon
+const xIcon = "<svg class='modal-icon lm-x-icon' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'><defs><style>.cls-1{fill:none; stroke-linecap:round;stroke-linejoin:round;}</style></defs><circle class='cls-1' cx='100' cy='100' r='90'/><line class='cls-1 lm-x-inner' x1='61.3' y1='61.3' x2='138.7' y2='138.7'/><line class='cls-1 lm-x-inner' x1='138.7' y1='61.3' x2='61.3' y2='138.7'/></svg>";
+/* File Upload */
+// Upload Time
+const uploadTime = 1750;
+// Drop zone element
+const dropZone = document.querySelector(".drop-zone");
+// File input
+const fileInput = document.querySelector(".lm-file");
+// Insert the image into these elements
+const insertLogoElements = document.querySelectorAll(".insert-photo");
+// Accepted file types
+const fileTypes = ['image/jpeg', 'image/png', 'image/svg+xml'];
+/* Upload Functions */
+// Insert logo
+function insertLogo(url) {
+    insertLogoElements.forEach((logoElem) => {
+        // Create an image element
+        const logoImg = document.createElement("img");
+        logoImg.src = url;
+        logoImg.classList.add("insert-logo-img");
+        logoImg.setAttribute("alt", "Uploaded Logo");
+        // Remove previous content from the logo elements
+        logoElem.innerHTML = "";
+        // Append logo to the elements
+        setTimeout(function () {
+            logoElem.appendChild(logoImg);
+        }, uploadTime);
+    });
+}
+// Insert upload icon
+function insertUploadIcon() {
+    dropZone.innerHTML = uploadIcon;
+}
+insertUploadIcon();
+// Remove upload icon active class
+function uploadIconActive() {
+    dropZone?.classList.remove("modal-icon-active");
+}
+// Insert X icon
+function insertXIcon() {
+    dropZone.innerHTML = xIcon;
+}
+// Checkmark icon
+function successfulUpload() {
+    dropZone.innerHTML = checkmarkIcon;
+    // Insert the upload icon and remove the classes
+    setTimeout(function () {
+        insertUploadIcon();
+        dropZone?.classList.remove("modal-icon-active");
+        dropZone?.classList.remove("modal-drop");
+    }, uploadTime);
+}
+/* Document drag events */
+document.documentElement.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    // Get the file dragged over the document
+    const file = e.dataTransfer?.items[0];
+    if (file && fileTypes.includes(file.type)) {
+        dropZone?.classList.add("modal-icon-active");
+    }
+    else if (file &&
+        !fileTypes.includes(file.type) &&
+        !dropZone?.contains(document.querySelector(".lm-x-icon"))) {
+        // Insert X icon
+        insertXIcon();
+    }
+});
+document.body.addEventListener('dragleave', function (e) {
+    // If the mouse is within the window - do nothing
+    if (e.clientX > 0 && e.clientY > 0 && e.clientX < window.innerWidth && e.clientY < window.innerHeight) {
+        return;
+    }
+    // Insert upload icon
+    insertUploadIcon();
+    // Remove the active class
+    uploadIconActive();
+});
+/* File on document drop */
+document.documentElement.addEventListener('drop', function (e) {
+    // Prevent from opening the file in another tab
+    e.preventDefault();
+    const eTarget = e.target;
+    if (!eTarget.closest(".drop-zone")) {
+        // Insert upload icon
+        insertUploadIcon();
+        // Remove the active class
+        uploadIconActive();
+    }
+});
+/* Drop zone drag events */
+dropZone?.addEventListener('dragover', function (e) {
+    // Get the file dragged over the document
+    const file = e.dataTransfer?.items[0];
+    if (file && fileTypes.includes(file.type)) {
+        dropZone?.classList.add("modal-drop");
+    }
+});
+dropZone?.addEventListener('dragleave', function (e) {
+    dropZone?.classList.remove("modal-drop");
+});
+// Functions called on logo upload
+function uploadFunctions(url) {
+    // Successful Upload
+    successfulUpload();
+    // Insert images into the page elements
+    insertLogo(url);
+}
+/* Drop zone drop event */
+dropZone?.addEventListener('drop', function (e) {
+    // Prevent from opening the file in another tab
+    e.preventDefault();
+    // Get the dropped file
+    const file = e.dataTransfer?.files[0];
+    if (file && fileTypes.includes(file.type)) {
+        // Create an URL object
+        const url = URL.createObjectURL(file);
+        uploadFunctions(url);
+    }
+    else {
+        // Insert upload icon
+        insertUploadIcon();
+    }
+});
+/* File upload via input */
+fileInput?.addEventListener("change", function () {
+    const file = fileInput.files[0];
+    if (file && fileTypes.includes(file.type)) {
+        // Create an URL object
+        const url = URL.createObjectURL(file);
+        uploadFunctions(url);
+    }
+    else if (file && !fileTypes.includes(file.type)) {
+        // Input (in HTML) excludes undesired file types, but just in case
+        // Insert X icon
+        insertXIcon();
+        // Insert upload icon after some time
+        setTimeout(function () {
+            insertUploadIcon();
+        }, 1000);
+    }
+});
+/* Delete File Button */
+const deleteFileBtn = document.querySelector(".delete-file");
+deleteFileBtn?.addEventListener("click", () => {
+    insertLogoElements.forEach((logoElem) => {
+        logoElem.innerHTML = "";
+    });
+});
