9dcb74243c6cbf9b2b7779a5f4b96a8cd1897497
Uverit <web.newhorizon@gmail.com>
2023-08-12 16:01:08 +0200

    v1.2.1 - Button Animation, Small Fixes
    Added animations to some of the nav buttons (on hover).
    
    Added text-wrap:balance to the additional section in the PDF document.
    
    Made small changes to the links at the bottom of the Settings section.

--- int_resources/ts/index.ts
+++ int_resources/ts/index.ts
@@ -4,7 +4,7 @@
     /* Program Settings */
 
 // Program Version
-const programVersion:string = "v1.2.0";
+const programVersion:string = "v1.2.1";
 
 
 
@@ -281,7 +281,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const programVersionElem:HTMLElement | null = document.querySelector(".program-version");
 
-    programVersionElem!.innerHTML = `<a href='https://github.com/newhorizonweb/Uverit-SkillPeak'>Uverit SkillPeak ${programVersion}</a>`;
+    programVersionElem!.innerHTML = `Uverit SkillPeak ${programVersion}`;
 
 
 
